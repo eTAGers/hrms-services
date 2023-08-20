eccabf7d1246b9305ef2c855104a94c65d565731
Harim2p <harihara.manibalan@m2pfintech.com>
2023-08-20 09:46:57 +0530

    store created added

--- app/query/user.query.js
+++ app/query/user.query.js
@@ -3,4 +3,6 @@ module.exports = {
     `SELECT userid, userName, email, mobile, hashedpassword FROM userdetails WHERE email = '${email}'`,
   loginByMobile: (mobile) =>
     `SELECT userid, userName, email, mobile, hashedpassword FROM userdetails WHERE mobile = '${mobile}'`,
+  store: (userid) =>
+    `SELECT storename, storecategory, storeid FROM store WHERE userid = '${userid}'`,
 };
